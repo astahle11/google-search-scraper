4e6d9e625220b479087db6555c474a78015e89c7
astahle <amnd.stahle@gmail.com>
2024-07-01 00:19:32 -0400

    working

--- main.py
+++ main.py
@@ -1,29 +1,55 @@
-from googlesearch import search
 from rich.console import Console
+from rich.traceback import install
+from rich.logging import RichHandler
+import logging
 import requests
-from requests_ip_rotator import ApiGateway
 from requests.adapters import HTTPAdapter
 import boto3
+from bs4 import BeautifulSoup
+from urllib.parse import urlparse
+from urllib.parse import parse_qs
+import json
+
+
+def extract_results(soup):
+    main = soup.select_one("#main")
+
+    res = []
+    for gdiv in main.select('.g, .fP1Qef'):
+        res.append(extract_section(gdiv))
+    return res
+
+def extract_section(gdiv):
+    # Getting our elements
+    title = gdiv.select_one('h3')
+    link = gdiv.select_one('a')
+    description = gdiv.find('.BNeawe')
+    return {
+        # Extract title's text only if text is found 
+        'title': title.text if title else None,
+
+        'link': extract_href(link['href']) if link else None,
+        'description': description.text if description else None
+    }
+
+def extract_href(href):
+    url = urlparse(href)
+    query = parse_qs(url.query)
+    if not ('q' in query and query['q'] and len(query['q']) > 0):
+        return None
+    return href, query['q'][0]
 
 def start_gatewayinit():
     
     gatewayiniturl = "https://qqrqi9ye9c.execute-api.us-east-2.amazonaws.com/dev"
     
-    gatewayinit = ApiGateway(gatewayiniturl)
-    gatewayinit.start()
-
     # Assign gateway to session
     session = requests.Session()
     
     # Mount the HTTPAdpater object to the session
     session.mount('https://', adapter)
-    
-    # Send request (IP will be randomised)
-    console.print(f'Sending test request to {gatewayiniturl}...')
-    response = session.get('https://google.com')
-    console.print(response.status_code)
-    
-    return gatewayinit, session
+
+    return session
     
 def start_gatewayshutdown(gatewayinit):
     
@@ -40,42 +66,48 @@ def display_message(console, message):
 
 if __name__ == '__main__':
     
-    file_path = "results.txt" 
-    data = []
-    
+    # Start the rich console
     console = Console()
     
-    #Authentication via aws-shell config file
+    # Make rich.console the default traceback handler
+    install()
+    FORMAT = "%(message)s"
+    logging.basicConfig(
+    level="ERROR", format=FORMAT, datefmt="[%X]", handlers=[RichHandler(rich_tracebacks=True)])
+    
+    # AWS STUFF
+    # Authentication via aws-shell config file
     session = boto3.Session(profile_name='default')
     session.resource('s3')
     
     # Create an HTTPAdapter object and configure connection pooling and retries
     adapter = HTTPAdapter(pool_connections=20, pool_maxsize=5, max_retries=3)
     
+    # Test out the gateway. 200 means the target was reached.
     session = start_gatewayinit()
-
+    # AWS STUFF DONE
+    
     try:
-        session = requests.Session()
-        session.mount('https://', adapter)
-        response = session.get("https://www.google.com/search?q=test")
+        results = []
+        
+        #query = input("Search: ")
+        query = 'cats'
+        file_path = "results.html" 
+        response = session.get("https://www.google.com/search?q=" + query)
+        
+        print("RESPONSE ", response.status_code)
+        
+        soup = BeautifulSoup(response.text, 'html.parser')
 
-        console.print(response.text)
+        res = extract_results(soup)
+       
+        with open('results.json', "w") as outfile:
+            json.dump(res, outfile)
         
-        write_response = response.text
         
-        for result in response:
-            
-            href = result  # Assuming result is a URL string
-            text = "N/A"  # Placeholder, as googlesearch typically returns just URLs
-            title = result
-            description = result
-            
-            with open(file_path, 'w', newline='') as text_file:
-                data.append({'link': href,'\n' 'text': text,'\n' 'title': title,'\n' 'description': description})
-                
     except EndProgramException:
-        console.print("Ending program.")
+        console.print_exception("Ending program.")
         
     except Exception as e:
-        console.print(f"An error occurred: {e}")
+        console.print_exception(f"An error occurred: {e}")
     
